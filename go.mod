58f043d67ce8ddbc4a4773228ea8ebd76ddbe5df
denis-pc <denispavlov1989@gmail.com>
2026-06-23 19:33:57 +0700

    add request logger

--- go.mod
+++ go.mod
@@ -3,15 +3,17 @@ module github.com/DenisPavlov/url-shortener
 go 1.26.3
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.3.0
 	github.com/stretchr/testify v1.11.1
+	go.uber.org/zap v1.28.0
 	resty.dev/v3 v3.0.0-rc.2
 )
 
 require (
-	github.com/caarlos0/env/v6 v6.10.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/net v0.43.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/cfg.go
+++ internal/config/cfg.go
@@ -9,16 +9,19 @@ import (
 type Cfg struct {
 	ServerAddress string `env:"SERVER_ADDRESS"`
 	BaseURL       string `env:"BASE_URL"`
+	LogLevel      string `env:"LOG_LEVEL"`
 }
 
 func MustLoad() Cfg {
 	serverAddress := flag.String("a", ":8080", "address and port to run server")
 	baseURL := flag.String("b", "http://localhost:8080", "result base url")
+	logLevel := flag.String("l", "debug", "log level")
 	flag.Parse()
 
 	cfg := Cfg{
 		ServerAddress: *serverAddress,
 		BaseURL:       *baseURL,
+		LogLevel:      *logLevel,
 	}
 
 	err := env.Parse(&cfg)
